21260dfbf209d14c4265e9cbe7885a0f40ffac30
Fernando Montoya <montogeek@gmail.com>
2013-04-23 05:20:47 -0500

    Actualizado configuracion base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => '127.0.0.1',
-			'database' => 'clontwitter',
+			'database' => 'database',
 			'username' => 'root',
 			'password' => '',
 			'charset'  => 'utf8',
